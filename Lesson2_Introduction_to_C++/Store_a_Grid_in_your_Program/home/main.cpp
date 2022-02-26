e44d0c9586ac62019927533503bdce6a5f081650
burakbicer <b.bicer505@gmail.com>
2022-02-26 14:23:47 +0300

    solution.cpp in "Store a Grid in your program" folder was deleted.

--- Lesson2_Introduction_to_C++/Store_a_Grid_in_your_Program/home/main.cpp
+++ Lesson2_Introduction_to_C++/Store_a_Grid_in_your_Program/home/main.cpp
@@ -5,12 +5,6 @@ using std::cout;
 
 int main() {
   
-  //  {{0, 1, 0, 0, 0, 0},
-  //   {0, 1, 0, 0, 0, 0},
-  //   {0, 1, 0, 0, 0, 0},
-  //   {0, 1, 0, 0, 0, 0},
-  //   {0, 0, 0, 0, 1, 0}} 
-  
   // TODO: Declare a "board" variable here, and store
   // the data provided above.
 
